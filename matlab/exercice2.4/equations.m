03d3c33b0e2fb2cfae1d9f89c6691b878e24e84a
Thierry Chappuis <tchappui@gmail.com>
2016-10-16 20:13:28 +0200

    Inversion de deux fichiers matlab dans l'exercice 2.4

---
+++ matlab/exercice2.4/equations.m
@@ -0,0 +1,27 @@
+function dvariables = equations(t, variables, A, B, D):
+    % On commence par récupérer les variables dans le bon ordre
+    u = variables(1);
+    v = variables(2);
+    x = variables(3);
+    y = variables(4);
+    z = variables(5);
+
+  
+    % On définit r comme dans la donnée
+    r = A * u * x - B * y * z;
+    
+    % Si t est supérieur à 4000, D prend la valeur de 0
+    if t > 4000
+        D = 0;
+    end
+        
+    % On définit les équations différencielles dans le bon ordre
+    dudt = - r - D/v * u;
+    dvdt = D;
+    dxdt = - r + D/v * (10.93 - x);
+    dydt = r - D/v * y;
+    dzdt = r - D/v * z;
+    
+    % On retourne les équations différence sous la forme d'un vecteur colonne
+    dvariables = [dudt; dvdt; dxdt; dydt; dzdt];
+end
---
+++ matlab/exercice2.4/solution.m
@@ -0,0 +1,42 @@
+% Houskeeping
+clc;
+clear
+
+% Définition des constantes
+A = 0.00009
+B = 0.0000833
+D = 0.05
+
+% Définition des conditions initiales
+u0 = 7.72
+v0 = 200
+x0 = 0
+y0 = 0
+z0 = 0
+
+% Comme on ne peut passer qu'une seule condition au solveur, on encapsule les
+% conditions dans un vecteur.
+variables0 = [u0, v0, x0, y0, z0];
+tspan = 0 : 1 : 5000;
+
+% On appelle le solveur qui s'appelle ode45() en Matlab
+[t, valeurs] = ode45(@(t, variables) equations(t,variables,A,B,D), tspan, variables0);
+
+u = valeurs(:,1);
+v = valeurs(:,2);
+x = valeurs(:,3);
+y = valeurs(:,4);
+z = valeurs(:,5);
+
+% On termine par l'affichage des graphiques
+figure(1);
+plot(t, u, t, x, t, y, t, z);
+xlabel('t (s)');
+ylabel('u,x,y,z');
+legend('u','x','y','z');
+
+figure(2);
+plot(t, v);
+xlabel('t (s)');
+ylabel('v');
+
